aab09d9a5851ef5380800d6d73c1731f3bafe9a3
Ubuntu <ubuntu@ip-172-31-33-90.eu-west-2.compute.internal>
2021-12-21 20:12:42 +0000

    splatt expression and for loops

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,17 +1,49 @@
 terraform {
-  required_providers{
+  required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~>2.15.0" // ~> 2.xx 
     }
   }
 }
 
 provider "docker" {
-  
+
 }
 
-resource "docker_image" "nodered_image"{
+resource "random_string" "random" {
+  count = 2 // 
+  length = 4
+  special = false
+  upper = false
+}
+
+
+resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
+}
+
+
+resource "docker_container" "nodered_container" {
+  count = 2
+  name  = join("-", ["nodered",random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    # external = 1880
+  }
+}
+
+
+
+
+output "ip_address" {
+  # for_each = docker_container.nodered_container[*]
+  value = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address, i.ports[0]["external"]])]
+  description = "The IP address and external port of  the container"
+}
+
+output "container-name" {
+  value = docker_container.nodered_container[*].name
 }
 
